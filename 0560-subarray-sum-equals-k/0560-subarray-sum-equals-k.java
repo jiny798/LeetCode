b261dc32cb0823320d4ce58637b1a558de755cbd
박진영 <122027452+jiny798@users.noreply.github.com>
2023-11-13 12:40:22 +0900

    Time: 1584 ms (5.11%), Space: 54.8 MB (6.20%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -9,16 +9,45 @@ class Solution {
         List<Integer> edgeList = new ArrayList<>();
         edgeList.add(-1);
         map.put(0,edgeList);
-        
+        // 1 2 3
         for(int i = 0 ; i < nums.length ; i++){
-            sum += nums[i];
-            if(map.get(sum - k) != null){
-                answer += map.get(sum-k).size();
-            }
+            sum += nums[i];   
+            arr[i] = sum; // 1 3 6 
             List<Integer> list = map.getOrDefault(sum, new ArrayList<>());
             list.add(i);
             map.put(sum,list);
         }
+        for(int i = -1 ; i < nums.length ; i++){
+            int n ;
+            if(i == -1){
+               n = 0; 
+            }else{
+               n = arr[i]; // 3 
+            }
+            List<Integer> flist = map.get(n);
+            flist.remove(0);
+            int cnt = 0 ;
+            if(map.get(n+k) != null){
+                List<Integer> tlist = map.get(n+k);
+                for(Integer num : tlist){
+                    if(i < num){
+                        cnt++;
+                    }
+                }
+            }
+            answer+=cnt;
+        }
+        
+        
+        // for(int i = 0 ; i < nums.length ; i++){
+        //     sum += nums[i];
+        //     if(map.get(sum - k) != null){
+        //         answer += map.get(sum-k).size();
+        //     }
+        //     List<Integer> list = map.getOrDefault(sum, new ArrayList<>());
+        //     list.add(i);
+        //     map.put(sum,list);
+        // }
      
         
         return answer;
