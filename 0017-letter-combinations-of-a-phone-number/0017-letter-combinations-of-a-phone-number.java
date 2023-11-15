372e7881825432a533b4753e1f8e4fdeebcd0fbd
박진영 <122027452+jiny798@users.noreply.github.com>
2023-11-15 10:49:27 +0900

    Time: 1 ms (79.73%), Space: 40.6 MB (94.60%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+    public static List<String> list;
+    public static Map<Integer,String> map;
+    public static String input;
+    public List<String> letterCombinations(String digits) {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        input = digits;
+        init(map);
+        BT(0,"");
+        return list; 
+    }
+    
+    public void BT(int index, String str){
+        if(index == input.length()){
+            if(index == 0) return;
+            list.add(str);
+            return;
+        }      
+        String s = map.get(Character.getNumericValue(input.charAt(index))); // abc
+        for(int i = 0 ; i < s.length() ; i++){
+            BT(index+1, str + String.valueOf(s.charAt(i))); // a
+        }               
+    }
+    
+    
+    public void init(Map<Integer,String> map){        
+        map.put(2,"abc");
+        map.put(3,"def");
+        map.put(4,"ghi");
+        map.put(5,"jkl");
+        map.put(6,"mno");
+        map.put(7,"pqrs");
+        map.put(8,"tuv");
+        map.put(9,"wxyz");        
+    }
+}
