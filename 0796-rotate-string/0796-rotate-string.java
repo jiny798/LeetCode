bfb425b74b72d5bb4729edaea64cf43b3785bcf6
박진영 <122027452+jiny798@users.noreply.github.com>
2023-11-17 13:32:51 +0900

    Time: 1 ms (32.92%), Space: 40.9 MB (18.19%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() > goal.length()){
+            return false;
+        }
+        
+        String haystack = s + s;
+        Set<String> set = new HashSet<>();
+        set.add(goal);
+        int length = goal.length();
+        int answer = -1 ;
+        for(int i = 0 ; i <= haystack.length() - length ; i++ ){
+            String str = haystack.substring(i,i+length);
+            if(set.contains(str)){
+                answer = i;
+                break;
+            }
+        }
+        
+        if(answer == -1){
+            return false;
+        }
+        return true;
+        
+    }
+}
