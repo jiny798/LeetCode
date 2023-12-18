580f4aee4bbddbdc711b0849809f06913391f52e
박진영 <122027452+jiny798@users.noreply.github.com>
2023-12-18 17:12:42 +0900

    Time: 153 ms (49.57%), Space: 41.3 MB (26.53%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -7,6 +7,7 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         answer = false;
         visit = new boolean[board.length][board[0].length];
+        
         for(int i = 0 ; i < board.length ; i++){
             for(int j = 0 ; j < board[0].length ; j++){
                 if(board[i][j] == word.charAt(0) ) { // A
