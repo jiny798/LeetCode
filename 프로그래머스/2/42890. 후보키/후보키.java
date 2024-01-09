1433b03cad12482b7f8e106f7132b0200087e63e
박진영 <122027452+jiny798@users.noreply.github.com>
2024-01-09 11:53:28 +0900

    [level 2] Title: 후보키, Time: 17.66 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,86 +1,81 @@
 import java.util.*;
-class Solution {
-    public static int N =0 ;
-    public static Map<String,Integer> duplMap;
-    public static List<String> answer;
-     public static Set<String> set;
-    
+class Solution {         
     public int solution(String[][] relation) {
-        boolean[] visit = new boolean[relation[0].length]; // 4개
-        answer = new ArrayList<>();
-        BT(0,visit,relation); // 유일성 체크 끝
+        Set<String> set = new HashSet<>(); 
+        Set<String> uniqueSet = new HashSet<>();
+        int answer = 0 ;
+        int[] visit = new int[relation[0].length];
+        BT(0,visit,set);
         
-        // 0110 1100 0100
-        for(int i = 0 ; i < answer.size() ; i++) { // 0110
-            String str1 = answer.get(i);
-            int n1 = Integer.parseInt(str1,2);
+        Iterator<String> it = set.iterator();
+        while(it.hasNext()) {
+            String next = it.next();
+            boolean isUnique = isUnique(relation, next); // 1101
+            if(isUnique) {
+                uniqueSet.add(next);
+            }            
+        }
+        
+        Iterator<String> it2 = uniqueSet.iterator();
+        
+        while(it2.hasNext()) {
+            String next = it2.next();
+            Iterator<String> it3 = uniqueSet.iterator();
             boolean tag = true;
-            for(int j = 0 ; j < answer.size() ; j++) {
-                if(i==j) continue;
-                String str2 = answer.get(j);
-                int n2 = Integer.parseInt(str2,2);
-                if( (n1 & n2) == n2) { // n1은 n2를 포함하므로 n1은 최소성 만족하지 않음
-                    tag = false;
-                    break;
-                }                
-            } 
+            while(it3.hasNext()) {
+                String inNext = it3.next();
+                if(next.equals(inNext)){
+                    continue;
+                }
+                int a = Integer.parseInt(next,2);
+                int b = Integer.parseInt(inNext,2);
+                if((a&b) == b) {
+                   tag = false; 
+                }
+            }
             if(tag) {
-                N++;
+                answer++;
             }
         }
         
-            
-        return N;
+        
+        
+        return answer;
     }
    
-    private void BT(int n , boolean[] visit, String[][] relation) {
-        if(n == visit.length){
-            checkUnique(visit,relation);
+    
+    private void BT(int n, int[] visit, Set<String> set) {
+        String str = "";
+        if(n == visit.length) {
+            for(int i = 0 ; i < visit.length ; i++ ) {
+                str += visit[i];
+            }
+            set.add(str);
             return;
-        }                
-        visit[n] = true;
-        BT(n+1,visit,relation);
-        visit[n] = false;                 
-        BT(n+1,visit,relation);        
+        }
+        visit[n] = 1;
+        BT(n+1, visit, set);
+        visit[n] = 0;
+        BT(n+1, visit, set);
     }
     
-    private void checkUnique(boolean[] visit, String[][] relation) {
-        duplMap = new HashMap<>();
-        for(int j = 0 ; j < relation.length ; j++) { // 모든 relation 반복 
-            String key = "";
-            for(int k = 0 ; k < relation[0].length ; k++) {
-                if(visit[k] == true) {
-                    key += relation[j][k] + ".";
-                }
-            }     
-            // System.out.println(key);
-            if(duplMap.get(key) == null) {      
-                duplMap.put(key,1);
+    private boolean isUnique(String[][] relation, String visit ) {
+        Set<String> uSet = new HashSet<>();
+        for(int i = 0 ; i < relation.length ; i++ ) {
+            String str = "";
+            for(int j = 0 ; j < visit.length() ; j++ ) {
+                if(visit.charAt(j) == '1') {
+                    str += relation[i][j] +"," ;
+                }   
+            }
+            if(!uSet.contains(str)){
+                uSet.add(str);
             }else{
-                duplMap.put(key, duplMap.get(key)+1);
-            }      
-        }
-        
-        for(Map.Entry<String,Integer> entry : duplMap.entrySet()) {
-            if(entry.getValue() >= 2){
-                // System.out.println(entry.getKey()); // 하나라도 2개 이상이 있으면 나가고
-                return;
+                return false;
             }
         }
-        String str = "";
-        // boolean[] tempVisit = new boolean[visit.length];
-        for(int i=0;i<visit.length;i++) { // 유일성을 만족하는 조합            
-            if(visit[i] == true) {
-                str += "1";
-            }else {
-                str += "0";
-            }
-            
-        }
-        answer.add(str);
-
-        
+        return true;
     }
-    // 1 1 0 0
-    // 0 1 0 0
+        
+    
 }
